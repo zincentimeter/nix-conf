0cdac34d76ef6a748356cd30b2c3670bb9109304
Shinri Kosame <shinri_kosame@outlook.com>
2024-03-20 23:06:01 +0800

    locale

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [ "all" ];
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
