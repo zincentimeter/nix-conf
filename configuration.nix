e3c1e26f2dd486bc13abae947a119bf21c87f5ab
zincentimeter <zincentimeter@outlook.com>
2024-03-23 02:52:59 +0800

    add alias, mv hardware to hardware/

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,11 @@
       ./disko.nix
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # Home folder (the ./home/default.nix will be imported)
+      # Home folder (./home/default.nix will be imported)
       ./home
+      # Sound / Graphic cards / Printers / Peripherals / etc.
+      # (./hardware/default.nix will be imported)
+      ./hardware
     ];
 
   sops = {
@@ -61,8 +64,10 @@
     addons = with pkgs; [ fcitx5-rime fcitx5-mozc fcitx5-gtk ];
   };
 
+  # Proxy
   services.v2raya.enable = true;
 
+  # Fingerprint
   services.fprintd.enable = true;
   security.pam.services = 
     builtins.mapAttrs (n: v: v // {
@@ -125,85 +130,6 @@
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Sound configuration
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # Enable trackpoint
-  hardware.trackpoint = {
-    enable = true;
-    emulateWheel = true;
-  };
-
-  # Graphic settings
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    powerManagement = {
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    	enable = false;
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-	    finegrained = false;
-    };
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    # shinri: my p16v has NVIDIA RTX A500 Laptop GPU, which comes with pci id 0x25BD
-    # shinri: that means it can use the open one.
-    open = true;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    # Hybrid Graphics (Nvidia Optimus PRIME)
-    prime = {
-      # Optimus Option C: Reverse Sync Mode (Experimental)
-      reverseSync.enable = true;
-      # Enable if using an external GPU
-      allowExternalGpu = false;
-
-      # Gathered from `lscpi -c display`
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.shinri = {
@@ -328,6 +254,7 @@
   # Shell aliases
   environment.shellAliases = {
     gnix = "git --work-tree=${config.users.users.shinri.home}/nix-conf";
+    ns = "sudo nixos-rebuild switch";
   };
 
   # List services that you want to enable:
