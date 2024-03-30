ded13bc0c2065e3c30b0ca1af1465dbce2da3f2d
zincentimeter <zincentimeter@outlook.com>
2024-03-31 05:41:33 +0800

    offloading vlc to use gpu

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,32 @@
 { config, lib, pkgs, ... }:
-
+let
+  nvidiaOffloadSetting = [
+    "--set __NV_PRIME_RENDER_OFFLOAD 1"
+    "--set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0"
+    "--set __GLX_VENDOR_LIBRARY_NAME nvidia"
+    "--set __VK_LAYER_NV_optimus NVIDIA_only"
+  ];
+  # `qtWrapperArgs` only works for qt apps!
+  # Read the derivative's source before apply override attribute!
+  # We may even not see a warning on our overrided variables not being used.
+  # Also, be sure to check the target of building
+  # by going to the directory of newly-built one:
+  # > readlink -f <the_binary_name>
+  # check the file of that wrapper to see if it REALLY contains these variables.
+  syncplayGpuOffloaded =
+    pkgs.syncplay.overrideAttrs (finalAttrs: previousAttrs:
+      {
+        # https://github.com/NixOS/nixpkgs/issues/27769#issuecomment-318906426
+        qtWrapperArgs = nvidiaOffloadSetting;
+      }
+    );
+  vlcGpuOffloaded =
+    pkgs.vlc.overrideAttrs (finalAttrs: previousAttrs:
+      {
+        qtWrapperArgs = nvidiaOffloadSetting;
+      }
+    );
+in
 {
   # Program configured with home-manager, but as a NixOS module!
   home-manager = {
@@ -30,7 +57,8 @@
 
         # watchalong
         syncplay
-        vlc
+      ] ++ [
+        vlcGpuOffloaded
       ];
 
       imports = [
