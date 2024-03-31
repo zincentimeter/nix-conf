9a9fa6f2e6af774d24f06a50b498052a74e3acbf
zincentimeter <zincentimeter@outlook.com>
2024-04-01 02:09:16 +0800

    load rclone automatically

--- home/onedrive.nix
+++ home/onedrive.nix
@@ -2,7 +2,7 @@
 
 let
   # The mount path of my OneDrive
-  mountPath = "${config.home.homeDirectory}/OneDrive";
+  mountRootPath = "${config.home.homeDirectory}/OneDrive";
   configName = "rclone/config.toml";
   configPath = "${config.xdg.configHome}/${configName}";
 in
@@ -14,26 +14,46 @@ in
   # > https://github.com/oxalica/onedrive-fuse/blob/main/README.md
   home.packages = with pkgs; [
     rclone
+    fuse
   ];
 
   # The config is saved at ${configPath}. since its managed by rclone
   # i will not take over it.
   # To configure, use /var/secrets/
   # "rclone" is the name of the new service we'll be creating
-  systemd.user.services."rclone" = {
-    Unit = {
-      After = ["network.target" "sound.target"];
-      Description = "Mount Microsoft OneDrive storage as FUSE filesystem";
-    };
-    # set it to ["default.target"] to make a unit start by default when the user <name> logs on.
-    Install.WantedBy = [ "default.target" ];
-    Service = {
-      # see systemd man pages for more information on the various options for "Type": "notify"
-      # specifies that this is a service that waits for notification from its predecessor (declared in
-      # `after=`) before starting
-      Type = "notify";
-      ExecStart = ''${pkgs.rclone}/bin/rclone mount ${mountPath}'';
-      ExecStop = ''${pkgs.rclone}/bin/fusermount -u ${mountPath}'';
-    };
+
+  systemd.user.services = 
+    let 
+      mkRcloneService = ({source, target} : {
+        Unit = {
+          After = [ "network.target" "sound.target"  ];
+          Description = "Mount Microsoft OneDrive storage as FUSE filesystem, ${source} -> ${target}";
+        };
+        # set it to ["default.target"] to make a unit start by default when the user <name> logs on.
+        Install.WantedBy = [ "xdg-desktop-portal.service" ];
+        Service = {
+          # see systemd man pages for more information on the various options for "Type": "notify"
+          # specifies that this is a service that waits for notification from its predecessor (declared in
+          # `after=`) before starting
+          Type = "notify";
+
+          ExecStart =
+          let
+            vfsCacheOption = "--vfs-cache-mode=writes";
+            mountCommand = "${pkgs.rclone}/bin/rclone mount ${vfsCacheOption}";
+          in
+          ''
+            ${mountCommand} ${source} ${target}
+          '';
+          ExecStop = ''${pkgs.fuse}/bin/fusermount -u ${target}'';
+        };
+      });
+    in
+  {
+    "onedrive@root" = mkRcloneService { source = "OneDrive:"; target = mountRootPath; };
+    "onedrive@videos" = mkRcloneService { source = "OneDrive:视频/"; target = config.xdg.userDirs.videos; };
+    "onedrive@pictures" = mkRcloneService { source = "OneDrive:图片/"; target = config.xdg.userDirs.pictures; };
+    "onedrive@documents" = mkRcloneService { source = "OneDrive:Documents/"; target = config.xdg.userDirs.documents; };
+    "onedrive@music" = mkRcloneService { source = "OneDrive:music/"; target = config.xdg.userDirs.music; };
   };
 }
