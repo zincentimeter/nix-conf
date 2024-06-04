4fe52bd180850793df4efd93941fd211c70b8ab7
zincentimeter <zincentimeter@outlook.com>
2024-06-04 20:56:35 +0800

    fonts update and config

--- system/font.nix
+++ system/font.nix
@@ -2,16 +2,36 @@
 
 {
   # Font
+
+  fonts.enableDefaultPackages = true;
+
   fonts.packages = with pkgs; [
     redhat-official-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     nerdfonts
   ];
+
+  fonts.fontconfig.enable = true;
 
   fonts.fontconfig.defaultFonts = {
     monospace = [
       "Red Hat Mono"
       "DejaVu Sans Mono"
+      "Noto Sans Mono CJK TC"
+      "Noto Sans Mono CJK SC"
+    ];
+
+    serif = [
+      "DejaVu Sans"
+      "Noto Serif CJK TC"
+      "Noto Serif CJK SC"
+    ];
+
+    sansSerif = [
+      "DejaVu Sans"
+      "Noto Sans CJK TC"
+      "Noto Sans CJK SC"
     ];
   };
 }
