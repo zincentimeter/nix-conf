cccfdc740694898b30d4d27f752d123e7e529d87
zincentimeter <zincentimeter@outlook.com>
2024-07-09 11:45:20 +0800

    flake: add comments / prepare for impermanence

--- flake.nix
+++ flake.nix
@@ -4,18 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # dependency of lanzaboote
     flake-utils.url = "github:numtide/flake-utils";
 
+    # dependency of lanzaboote
     crane = {
       url = "github:ipetkov/crane/v0.16.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # secrets management
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # secure boot
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.3.0";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,29 +28,37 @@
       inputs.crane.follows = "crane";
     };
 
+    # declarative disk partition
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # managing the /home folder
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # managing KDE plasma GUI profile declaratively
     plasma-manager = {
       url = "github:pjones/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
 
+    # Lets you choose what files and directories you want to keep
+    # between reboots - the rest are thrown away.
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-stable, ... } @ inputs:
   {
     nixosConfigurations = {
       # Use `rec` to use `system` variable inside overlays
-      "thinkpad-p16v" = nixpkgs.lib.nixosSystem rec {
+      "thinkpad-p16v" = nixpkgs.lib.nixosSystem {
 
         system = "x86_64-linux";
 	specialArgs = {
@@ -86,6 +99,7 @@
           inputs.disko.nixosModules.disko
           inputs.home-manager.nixosModules.home-manager
           inputs.sops-nix.nixosModules.sops
+          inputs.impermanence.nixosModules.impermanence
 
         ];
       };
