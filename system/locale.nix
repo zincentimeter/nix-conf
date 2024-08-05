6319dc4fdfae1954b9f92ccaee7ae871ea144f28
zincentimeter <zincentimeter@outlook.com>
2024-08-05 17:33:13 +0800

    system-i18n: apply nixpkgs config change

--- system/locale.nix
+++ system/locale.nix
@@ -21,7 +21,8 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.enable = true;
+  i18n.inputMethod.type = "fcitx5";
   i18n.inputMethod.fcitx5 = {
     addons = with pkgs; [ fcitx5-rime fcitx5-mozc fcitx5-gtk ];
     waylandFrontend = true;
