c32aaaba11d71c1b38f03800b7d8e714599787cc
zincentimeter <zincentimeter@outlook.com>
2024-08-17 02:47:16 +0800

    disko, misc: indent change

--- system/disko.nix
+++ system/disko.nix
@@ -1,35 +1,33 @@
 {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/nvme1n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme1n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-            root = {
-              end = "-16GiB";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
+          };
+          root = {
+            end = "-16GiB";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
-            swap = {
-              size = "100%";
-              content = {
-                type = "swap";
-                resumeDevice = true;
-              };
+          };
+          swap = {
+            size = "100%";
+            content = {
+              type = "swap";
+              resumeDevice = true;
             };
           };
         };
