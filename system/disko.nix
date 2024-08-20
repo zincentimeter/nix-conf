ef3cc3d7e55e546231beff28620e6b603661bbd2
zincentimeter <zincentimeter@outlook.com>
2024-08-21 06:35:12 +0800

    disko, misc: prepare tmpfs, disabled for now

--- system/disko.nix
+++ system/disko.nix
@@ -1,5 +1,6 @@
 {
   disko.devices = {
+
     disk.main = {
       type = "disk";
       device = "/dev/nvme1n1";
@@ -28,11 +29,26 @@
             content = {
               type = "swap";
               resumeDevice = true;
-            };
-          };
-        };
-      };
-    };
-  };
+            }; # content
+          }; # swap
+        }; # partitions
+      }; # content
+    }; # disk.main
+    /*
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        # The default, when neither size nor nr_blocks is specified, is size=50%.
+        "default"
+        # https://serverfault.com/q/644468
+        "noatime"
+        # 7=rwx, 5=r-x, 5=r-x
+        # set mode to 755, otherwise systemd will set it to 777,
+        # which cause problems.
+        "mode=755"
+      ]; # mountOptions
+    }; # nodev."/"
+    */
+  }; # disko.devices
 }
 
