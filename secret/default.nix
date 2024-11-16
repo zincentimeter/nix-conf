0a7b369cad184db77f43d7728e0b3ad276856e12
zincentimeter <zincentimeter@outlook.com>
2024-11-16 20:51:11 +0800

    [public] sosp, secret: replace invalidated secrets
    - add azure token, preparing for oama's oauth

--- secret/default.nix
+++ secret/default.nix
@@ -18,16 +18,28 @@
         }
       ) {
         # Secrets listed here are managed and readable by user <shinri>.
-        "rclone_azure_token" = {};
-        "rclone_client_secret_value" = {};
-        "rclone_client_secret_secret_id" = {};
-        "outlook_password" = {};
         "campus_password" = {};
+        "azure_client_id" = {};
+        "azure_client_secret" = {};
       } // {
         # Secrets listed here requires elevated permission.
         "github_access_token" = {};
-        "onedrive_fuse_azure_token" = {};
       };
+
+    templates."oama_config.yaml" = {
+      mode = "0440";
+      owner = config.users.users.shinri.name;
+      group = config.users.users.shinri.group;
+      content = ''
+        encryption:
+          tag: KEYRING
+        services:
+          microsoft:
+            client_id: ${config.sops.placeholder.azure_client_id}
+            client_secret: ${config.sops.placeholder.azure_client_secret}
+      '';
+    };
+
     templates."nix_access_token.conf".content = ''
       access-tokens = github.com=${config.sops.placeholder.github_access_token}
     '';
