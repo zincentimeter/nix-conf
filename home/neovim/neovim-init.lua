f69455f85819112f47ab917e0af84caac6ec9fd2
zincentimeter <zincentimeter@outlook.com>
2025-03-26 02:03:54 +0800

    home-neovim: use default mapping for cmdline

--- home/neovim/neovim-init.lua
+++ home/neovim/neovim-init.lua
@@ -183,6 +183,7 @@ cmp.setup({
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
   }
@@ -190,6 +191,7 @@ cmp.setup.cmdline({ '/', '?' }, {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'cmdline' }
   }, {
