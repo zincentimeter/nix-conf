a78e7489390415ed4880e4837f31545474c42053
zincentimeter <zincentimeter@outlook.com>
2025-11-26 01:50:06 +0800

    home-git: extraConfig -> settings

--- home/git.nix
+++ home/git.nix
@@ -4,7 +4,7 @@
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
-    extraConfig = {
+    settings = {
       sendemail = {
         identity = "outlook";
         thread = true;
