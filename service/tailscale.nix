f94276e8b93a8a3fab4c488a3a8ec6508b6d12fe
zincentimeter <zincentimeter@outlook.com>
2025-11-30 17:45:26 +0800

    service: re-enable tailscale

--- service/tailscale.nix
+++ service/tailscale.nix
@@ -4,7 +4,7 @@
   # after that, run: 
   # > tailscale up --accept-dns=false --hostname=thinkpad-p16v-nixos --login-server=https://wired.side7.click:443
   services.tailscale = {
-    enable = false;
+    enable = true;
     useRoutingFeatures = "client";
     openFirewall = true;
   };
