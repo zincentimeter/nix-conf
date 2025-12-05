7395ebbeb4f42023f3eb594993d99b6fda7d13e2
zincentimeter <zincentimeter@outlook.com>
2025-12-06 04:42:45 +0800

    Revert "feat(home-terminal): add opacity control for kitty"
    This reverts commit e51348dbd707584f6e58e638f585ec98129eabc9.

--- home/terminal.nix
+++ home/terminal.nix
@@ -6,9 +6,6 @@
     # See: https://sw.kovidgoyal.net/kitty/conf.html
     settings = {
       cursor_blink_interval = 0;
-      background_opacity = 0.5;
-      background_blur = 30;
-      dynamic_background_opacity = true;
     };
     shellIntegration.enableFishIntegration = true;
   };
