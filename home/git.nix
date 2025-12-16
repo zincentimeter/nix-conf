c3dad5431f8591818140f9f2a8c974fb1cd9c45b
zincentimeter <zincentimeter@outlook.com>
2025-12-17 02:20:33 +0800

    feat(home-git): enable signing with SSH key as default

--- home/git.nix
+++ home/git.nix
@@ -1,15 +1,40 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  outlook = with config.accounts.email.accounts.outlook; {
+    inherit address name;
+    # https://meta.sr.ht/~zincentimeter.keys
+    ssh = {
+      publicKey = ''
+        ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEaCJT81R7+czV8BmMYBM2N+aPbraUdmTh0Sjz5USDJX
+      '';
+      privateKeyPath = "${config.home.homeDirectory}/.ssh/thinkpad_nixos";
+    };
+  };
+in
 {
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
     settings = {
       sendemail = {
-        identity = "outlook";
+        identity = outlook.name;
         thread = true;
         confirm = "auto";
       };
+      # https://github.com/LukeChannings/dotfiles/blob/57fe7e1c188188c88092d655dbc897b3b6b54f1e/config/git/home.nix#L78
+      gpg.ssh.allowedSignersFile = builtins.toString ( # package path to string
+        pkgs.writeText "allowed_signers" (
+          lib.concatLines (
+            [ "${outlook.address} ${outlook.ssh.publicKey}" ]
+          )
+        )
+      );
+    };
+    signing = {
+      signByDefault = true;
+      format = "ssh";
+      key = outlook.ssh.privateKeyPath;
     };
     lfs.enable = true;
   };
